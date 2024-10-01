629716892338b7f7c7ca43a4fd9b4e6f8dba5fa7
tuantranO4 <tuanxboxacc@gmail.com>
2024-10-01 19:54:15 +0200

    Week 4

--- WEB/practice03/script3.js
+++ WEB/practice03/script3.js
@@ -3,23 +3,29 @@ const H1 = document.querySelector('h1')
 const TABLE = document.querySelector('table')
 
 // Fill the h1 with the title
-H1.innerText = 'Mandalorian - Season 1'
+H1.innerText = 'Mandalorian - Season 1' //(check style3.css) 
 
 // Hardcode table header
 //let tableHeader = ["Title", "Summary"]
 
+//TABLE HEADER=========================================
 // Gather table header
-let tableHeader = Object.keys(episodes[0])
+let tableHeader = Object.keys(episodes[0]) //return the keys(id, url, name...) not value(chapter 1, 2019-xx-xx...)
+//why episodes[0]? doesnt matter, it just returns key(id, url...), also works with 1 2 3...
 
-
+//HEADER CELL MAKER=========================================
 // Display table header
-let tr = document.createElement('tr')
-tableHeader.forEach(key => {
-    let th = document.createElement('th')
-    th.innerText = key
-    tr.appendChild(th)
+let tr = document.createElement('tr') //create .js table row in memory (rationalise it in appendChild)
+tableHeader.forEach(key => { 
+    let th = document.createElement('th'); // Create a new table header cell (<th>) element
+    th.innerText = key; // Set the text content of the header cell to the current key
+    tr.appendChild(th); // Append the header cell to the row
 });
-TABLE.appendChild(tr)
+TABLE.appendChild(tr); // Append the populated table row to the actual table in the document
+//---------
+//Object.keys(episodes[0]) returns: ["id", "url", "name", "season", "number", "type", ...]
+//so the key in foreach loops iterate thru that returned array
+//---------
 
 // Create a table for the episodes - For manual header, with choosen elements
 /*
@@ -35,63 +41,71 @@ for (const episode of episodes) {
 }
 */
 
+//TABLE ROW POPULATING LOOP=========================================
 // Create table for all elements
+for (const episode of episodes) { //iterate through episodes const (data3.js)
+    let tr = document.createElement('tr') //create table row (vertical for each episode)
+    
+    for (const key in episode) {//no need for tableHeader because we directly accessing eposides. 
+        //const key: official syntax to access properties in for...in... loop 
+        const data = episode[key]; //store the corresponding value of key (url, airstamp...)
 
-for (const episode of episodes) {
-    let tr = document.createElement('tr')
-    for (const key in episode) {
-        const data = episode[key];
-        let td = document.createElement('td')
+        let td = document.createElement('td') //create table cell 'td' elems in memory (append later)
         
         // Optional task: Handle the different data types
         switch (key) {
             case 'url':
-                let a = document.createElement('a')
-                let icon = document.createElement('i');
-                icon.classList.add('fas', 'fa-link'); 
-                a.appendChild(icon);
-                a.href = data
-                td.appendChild(a)
+                let a = document.createElement('a') //anchor element (website hyperlink with href attribute)
+                let icon = document.createElement('i');//icon element 
+                icon.classList.add('fas', 'fa-link'); //style icon with css classes
+                a.appendChild(icon);//append icon to anchor
+                a.href = data //set href to data=ep.key[] (in this case key=url)
+                td.appendChild(a)//append anchor to table cell
                 break;
 
             case 'airstamp':
                 let date = new Date(data);
-                td.innerText = date.toLocaleString('hu-HU');
+                td.innerText = date.toLocaleString('hu-HU'); //Converts the airstamp string to a Date object.
+                //then formats it as a local string using Hungarian locale.
                 break;
 
             case 'runtime':
-                td.innerText = data + ' minutes'
+                td.innerText = data + ' minutes' //display runtime in minute
                 break;
 
             case 'rating':
-                let span = document.createElement('span')
+                let span = document.createElement('span') //display rating in astyled span
                 span.innerHTML = data
                 span.classList.add('rating')
-                td.appendChild(span)
+                td.appendChild(span) //besure to add them later if we not modify 'td' directly (like td.innerText)
                 break;
 
             case 'image':
-                let img = document.createElement('img')
-                img.src = data
-                img.alt = 'Episode Cover Image'
+                let img = document.createElement('img') //create img elem
+                img.src = data //souce of image
+                img.alt = 'Episode Cover Image' //alt text
                 td.appendChild(img)
                 break;
 
-            case 'summary':
+            case 'summary'://display summary innerhtml
                 td.innerHTML = data
                 td.classList.add('summary')
                 break;      
-                          
+                //td.classList.add() is a method used to add one or more CSS styles to an HTML element's class attribute.
+                //way to manipulate CSS (we use those styled elems in style3.css, and use .add for the picturisation)          
+                
             default:
-                td.innerText = data
+                td.innerText = data //default case (id, ep, season)
                 break;
         }        
-        tr.appendChild(td)
+        tr.appendChild(td)//shape up td on tr
     }
-    TABLE.appendChild(tr)
+    TABLE.appendChild(tr) //picturising tr (included td)
 }
 
+ 
 
+//INTERACTIVE CLICKER HANDLING===========================
 // Table Row click handler
 function handleTableRowClick() {
     // Check if the clicked <tr> contains any <th> elements
