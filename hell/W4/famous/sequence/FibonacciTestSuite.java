605a18646ebe5d3aaf670e4b2503b3bb657db027
tuantranO4 <tuanxboxacc@gmail.com>
2024-10-02 08:31:12 +0200

    31/08

--- hell/W4/famous/sequence/FibonacciTestSuite.java
+++ hell/W4/famous/sequence/FibonacciTestSuite.java
@@ -3,6 +3,8 @@ package famous.sequence;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+import FibonacciStructureTest;
+
 @Suite
 @SelectClasses({
     FibonacciStructureTest.class,
