f5589cd6f78be4048d47932b5fe77fbce70b49b5
Tuan <tuanxboxacc@gmail.com>
2024-11-05 16:15:03 +0100

    well

--- WEB/practice06/script6.js
+++ WEB/practice06/script6.js
@@ -1,37 +1,37 @@
 const CANVAS = document.querySelector("canvas")
-const CONTEXT = CANVAS.getContext('2d')
+const CONTEXT = CANVAS.getContext('2d')//retrieve the canvas context in 2D mode to access to all the 2D drawing methods, like fillRect(), drawImage(), and strokeRect()
 
 let planeImg = new Image()
 planeImg.src = "assets/plane.png"
 
-CANVAS.width = window.innerWidth
+CANVAS.width = window.innerWidth //canvas size to match the full width and height of the window, so the plane can move freely across the screen
 CANVAS.height = window.innerHeight
 
 let plane = {
     x: 0,
-    y: CANVAS.height / 2 - 63,
+    y: CANVAS.height / 2 - 63, //plane starts at the left edge and vertically centered
     w: 200,
-    h: 125,
-    vy: 0,
-    ay: 200
+    h: 125, //w,h: plane dimension
+    vy: 0, // is the rate at which the plane moves vertically
+    ay: 200 //ay (vertical acceleration) simulates gravity, pulling the plane downward when no other force is applied.
 }
 
-let lastFrame = performance.now()
+let lastFrame = performance.now() //lastFrame stores the timestamp of the previous frame. This is used to calculate deltaT
 
 function gameLoop() {
-    let now = performance.now()
-    let deltaT = (now - lastFrame) / 1000
+    let now = performance.now() // captures the current time in milliseconds
+    let deltaT = (now - lastFrame) / 1000 //Convert to seconds
 
     lastFrame = now
 
-    update(deltaT)
-    render()
-    requestAnimationFrame(gameLoop)
+    update(deltaT) //adjust the plane's position
+    render() //draw the plane at its updated location
+    requestAnimationFrame(gameLoop) //build-in func rAF
 }
 
-function update(deltaT) {
-    plane.vy += plane.ay * deltaT
-    plane.y += plane.vy * deltaT
+function update(deltaT) { //updates the plane’s velocity and position
+    plane.vy += plane.ay * deltaT // vertical velocity
+    plane.y += plane.vy * deltaT // location
 }
 
 function render() {
@@ -39,18 +39,18 @@ function render() {
     CONTEXT.strokeStyle = "black"
     CONTEXT.lineWidth = 2
 
-    CONTEXT.fillRect(0,0,CANVAS.width,CANVAS.height)
-    CONTEXT.strokeRect(plane.x,plane.y,plane.w,plane.h)
-    CONTEXT.drawImage(planeImg,plane.x,plane.y,plane.w,plane.h)
+    CONTEXT.fillRect(0,0,CANVAS.width,CANVAS.height) //draws the background
+    CONTEXT.strokeRect(plane.x,plane.y,plane.w,plane.h) //draws a rectangle outline around the plane’s position
+    CONTEXT.drawImage(planeImg,plane.x,plane.y,plane.w,plane.h) // draws the plane’s image at its current position
 }
 
 function handleFlight(e) {
-    if(e.code == "Space") {
+    if(e.code == "Space") { //listens for the "Space" key press
         e.preventDefault()
-        plane.vy = -200
+        plane.vy = -200 //pressing space repeatedly the player can "fly" the plane, counteracting gravity briefly
     }
 }
-
 document.addEventListener("keydown", handleFlight)
 // CANVAS.style.border="2px solid red"
-gameLoop()
+
+gameLoop() //Initiates the game by calling gameLoop()
